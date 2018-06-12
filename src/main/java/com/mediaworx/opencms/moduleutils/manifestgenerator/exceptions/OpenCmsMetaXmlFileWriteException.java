b5bf765bfc2f69fc2c1d394ac71333ca6852c0df
Lars Brandt <brandt@silpion.de>
2018-06-12 14:07:29 +0200

    fix wrong license

--- src/main/java/com/mediaworx/opencms/moduleutils/manifestgenerator/exceptions/OpenCmsMetaXmlFileWriteException.java
+++ src/main/java/com/mediaworx/opencms/moduleutils/manifestgenerator/exceptions/OpenCmsMetaXmlFileWriteException.java
@@ -1,32 +1,28 @@
-/*
- * This file is part of the OpenCms Module Manifest Generator by mediaworx.
- *
- * For further information about the OpenCms Module Manifest Generator,
- * please see the project website at GitHub:
- * https://github.com/mediaworx/opencms-manifestgenerator
- *
- * The OpenCms Module Manifest Generator is used by the OpenCms Plugin for
- * IntelliJ. For further information see the plugin's project site at GitHub:
- * https://github.com/mediaworx/opencms-intellijplugin
- *
- * Copyright (C) 2012-2014 mediaworx berlin AG (http://www.mediaworx.com)
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 3 of the License, or (at your
- * option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- */
+
 
 package com.mediaworx.opencms.moduleutils.manifestgenerator.exceptions;
+
+/*-
+ * #%L
+ * OpenCms Maven Plugin
+ * %%
+ * Copyright (C) 2017 - 2018 Silpion IT-Solutions GmbH (https://www.silpion.de/)
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
 
 /**
  * Exception used when there's an error writing the manifest file to the target
